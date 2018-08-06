4486c6ae730f8dc6b4b3128d7d52b03f47ccaf6b
hackingyseguridad <hackingyseguridad@hackingyseguridad.com>
2018-08-06 02:13:44 +0200

    Update dnslist.sh

--- dnslist.sh
+++ dnslist.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
-
+cat << "INFO"
+________    _______     _________.____     .___   ____________________
+\______ \   \      \   /   _____/|    |    |   | /   _____/\__    ___/
+ |    |  \  /   |   \  \_____  \ |    |    |   | \_____  \   |    |
+ |    `   \/    |    \ /        \|    |___ |   | /        \  |    |
+/_______  /\____|__  //_______  /|_______ \|___|/_______  /  |____|
+        \/         \/         \/         \/             \/
+                                                hackingyseguridad.con
+INFO
 if [ -z "$1" ]; then
         echo
-        echo "hackingyseguridad.com 2018"
         echo "Descubre subdominios.. "
         echo "Uso: $0 <dominio.com>"
         exit 0
 fi
+
+echo "==================="
+echo
 
 for n in `cat subdominios.txt`
 
