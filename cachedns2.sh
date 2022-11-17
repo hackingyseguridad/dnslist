5b3d127cb6eeceae7a7128080b34fcda3e4a46f3
hackingyseguridad <antonio_taboada@hotmail.es>
2022-11-17 21:06:23 +0100

    Update cachedns2.sh

--- cachedns2.sh
+++ cachedns2.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-# Version Beta 
+# Version Beta
 # Test respuesta de los dns cache
 # hacking y seguridad 2022
 # Uso.: #sh dnscaches.sh
 #
-API_HOST=$(</dev/urandom tr -dc a-z0-9|head -c32).edns.ip-api.com; API_IP=$(dig $API_HOST +short @80.58.0.33); curl -H "Host: $API_HOST" http://$API_IP/json
+while : ; do API_HOST=$(</dev/urandom tr -dc a-z0-9|head -c32).edns.ip-api.com; API_IP=$(dig $API_HOST +short @80.58.0.33); curl -H "Host: $API_HOST" http://$API_IP/json ; done
+
