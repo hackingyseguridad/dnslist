f6f09c8f04f786ddcabe6b4dc003c868c872ebc2
hackingyseguridad <antonio_taboada@hotmail.es>
2026-07-16 17:47:44 +0200

    Update actualizar.sh

--- actualizar.sh
+++ actualizar.sh
@@ -5,15 +5,15 @@ echo "##################################################################"
 echo
 echo "...."
 echo "....."
-wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/refs/heads/master/subdominios.txt -q -O subdoominios.txt  --inet4-only
+wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/refs/heads/master/subdominios.txt -q -O subdominios.txt  --inet4-only
 wc -l subdominios.txt
 echo ".."
 echo "..."
-wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/refs/heads/master/subdominios2.txt -q -O subdoominios2.txt  --inet4-only
+wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/refs/heads/master/subdominios2.txt -q -O subdominios2.txt  --inet4-only
 wc -l subdominios2.txt
 echo "...."
 echo "....."
-wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/refs/heads/master/subdominios3.txt -q -O subdoominios3.txt  --inet4-only
+wget https://raw.githubusercontent.com/hackingyseguridad/diccionarios/refs/heads/master/subdominios3.txt -q -O subdominios3.txt  --inet4-only
 wc -l subdominios3.txt
 echo "...."
 echo "....."
